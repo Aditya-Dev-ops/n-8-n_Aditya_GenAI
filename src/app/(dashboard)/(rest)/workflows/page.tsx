81f125a5e48640774cd3ea1831c267d871e6612d
Aditya-Dev-ops <aditya.developer.33@gmail.com>
2026-04-06 15:53:29 +0530

    set crud application for workflows

--- src/app/(dashboard)/(rest)/workflows/page.tsx
+++ src/app/(dashboard)/(rest)/workflows/page.tsx
@@ -1,10 +1,25 @@
 import { requireAuth } from "@/lib/auth-utils";
+import { HydrateClient } from "@/trpc/server";
+import { prefetchWorkflows } from "@/features/workflows/server/prefetch";
+import { ErrorBoundary } from "react-error-boundary";
+import { Suspense } from "react";
+import { WorkflowsContainer, WorkflowsList } from "@/features/workflows/components/workflows";
 
 const page = async () => {
   await requireAuth();
+  prefetchWorkflows();
+  
   return (
-    <div>Workflow Page</div>
+   <WorkflowsContainer>
+    <HydrateClient>
+       <ErrorBoundary fallback={<p>Error!</p>}>
+          <Suspense fallback={<p>Loading...</p>}>
+             <WorkflowsList/>
+          </Suspense>
+       </ErrorBoundary>
+    </HydrateClient>
+   </WorkflowsContainer>
   );
 }
 
-export default page
+export default page;
--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,34 +1,8 @@
-import {  createTRPCRouter, protectedProcedure } from '../init';
-import { prisma } from '@/lib/db';
-import {generateText } from "ai";
-import {google} from "@ai-sdk/google"
-import z from "zod";
+import { workflowsRouter } from "@/features/workflows/server/router";
+import { createTRPCRouter } from "../init";
 
 export const appRouter = createTRPCRouter({
- testAi: protectedProcedure.mutation(async ()=>{
-    const {text} = await generateText({
-        model: google("gemini-2.5-flash"),
-        prompt: "Write a vegetarian lasagna reccipe for 4 people"
-    });
-    return text;
- }),
- getWorkflows: protectedProcedure.query(({ctx})=> {
- console.log({userId : ctx.auth.user.id})
- return prisma.workflows.findMany({
-    where:{
-        id:ctx.auth.user.id,
-    }
- });
- }),  
- createWorkflow: protectedProcedure
-   .input(z.object({ name: z.string().min(1) }))
-   .mutation(async ({ ctx, input }) => {
-    return prisma.workflows.create({
-        data:{
-            name: input.name,
-        }
-    })
- }),
+    workflows: workflowsRouter,
 });
-// export type definition of API
+
 export type AppRouter = typeof appRouter;
